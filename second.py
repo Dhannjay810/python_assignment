bca7be7f5a58b52c2e695b86a09d58f51b64b0ec
Dhannjay810 <70064581+Dhannjay810@users.noreply.github.com>
2025-04-28 12:36:38 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,9 @@
+# Take user's first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate to form full name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print(f"Hello, {full_name}! Welcome to the python program.")
